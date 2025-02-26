3f3758bddd0e159b736329c87ef2a9efae873bf4
uditchauhan3 <uditchauhan72@gmail.com>
2025-02-26 22:22:32 +0530

    added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import { Sidebar } from "./_components/sidebar";
 import { Navbar } from "./_components/navbar";
 import { OrgSidebar } from "./_components/org-sidebar";
-
+// hey layout.tsx
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
