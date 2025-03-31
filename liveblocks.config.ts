876be5e4831fea52c2fa11cb6b49385c8ad07e11
Rohit-3413 <rohitguptarg6542@gmail.com>
2025-03-31 12:07:01 +0530

    key removed

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,7 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
   
 const client = createClient({
-  publicApiKey: "pk_dev_krCu9qYfEd28wKx2zZQJAyB7iqYMsv0XyUkFE76HJaSk6eRrMFStdMnKDShdzgNS",
+  publicApiKey: "",
   
 });
 
